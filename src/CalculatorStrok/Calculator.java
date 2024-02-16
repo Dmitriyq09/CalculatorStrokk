ff5707bc7c1ba03099a99667dcb149892e17f8ed
Dmitriyq09 <36569062+Dmitriyq09@users.noreply.github.com>
2024-02-16 19:26:13 +0300

    Updata Calculator

--- src/CalculatorStrok/Calculator.java
+++ src/CalculatorStrok/Calculator.java
@@ -9,6 +9,12 @@ public class Calculator {
         String str = scanner.nextLine();
         char action;
         String[] data;
+        extracted(str);
+    }
+
+    private static void extracted(String str) throws Exception {
+        char action;
+        String[] data;
         if (str.contains(" + ")) {
             data = str.split(" \\+ ");
             action = '+';
@@ -27,38 +33,39 @@ public class Calculator {
         if (action == '*' || action == '/'){
             if (data[1].contains("\"")) throw new Exception("Строчку можно делить или умножать только на число.");
         }
-        for (int i =0; i < data.length;i++){
+        for (int i = 0; i < data.length; i++) {
             data[i] = data[i].replace("\"", "");
         }
+
         if (action == '+') {
             printInQuotes(data[0] + data[1]);
         } else if (action == '*') {
             int multiplier = Integer.parseInt(data[1]);
             String result = "";
             for (int i = 0; i < multiplier; i++) {
-                result+=data[0];
+                result += data[0];
             }
             printInQuotes(result);
         } else if (action == '-') {
             int index = data[0].indexOf(data[1]);
-            if(index == -1){
+            if (index == -1) {
                 printInQuotes(data[0]);
-            }else{
+            } else {
                 String result = data[0].substring(0, index);
-                result+=data[0].substring(index+data[1].length());
+                result += data[0].substring(index + data[1].length());
                 printInQuotes(result);
             }
-        }else{
-            int newLen = data[0].length()/Integer.parseInt(data[1]);
-            String result = data[0].substring(0,newLen);
+        } else {
+            int newLen = data[0].length() / Integer.parseInt(data[1]);
+            String result = data[0].substring(0, newLen);
             printInQuotes(result);
         }
-
-
     }
+
     static void printInQuotes(String text){
+        System.out.println("\""+text+"\"");
         if (text.length()>40){
-            System.out.println(" "+text.substring(0,40)+"..."+" ");
+            System.out.println("\""+text.substring(0,40)+"..."+"\"");
         }
     }
 }
